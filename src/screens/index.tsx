e036da8fc88e41bce05236b91fd1900b02cd4483
Marcos <devmarcos7@gmail.com>
2025-05-06 11:04:55 -0300

    func de criar tarefas e listagem de tarefas finalizadas

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -5,12 +5,28 @@ import { Task } from "../components/Task";
 import { useState } from "react";
 import { TaskDTO } from "../dtos/TaskDTO";
 import { Empty } from "../components/Empty";
+import { uuid } from "../utils/uuid";
 
 export function HomeScreen() {
   const [tasks, setTasks] = useState<TaskDTO[]>([]);
+  const [newTask, setNewTask] = useState("");
+
+  function handleTaskAdd() {
+    if (newTask !== "" && newTask.length >= 5) {
+      setTasks((tasks) => [
+        ...tasks,
+        { id: uuid(), isCompleted: false, title: newTask.trim() },
+      ]);
+      setNewTask("");
+    }
+  }
   return (
     <View style={styles.container}>
-      <Header />
+      <Header
+        task={newTask}
+        onChangeText={setNewTask}
+        onPress={handleTaskAdd}
+      />
       <View style={styles.tasksContainer}>
         <View style={styles.info}>
           <View style={styles.row}>
